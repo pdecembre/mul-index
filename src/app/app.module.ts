5ee8e3af043ce08fd960bfb40fca701074004d6a
Pierre Decembre <pdecembre@gmail.com>
2017-06-22 15:35:55 -0400

    nothing much

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 @NgModule({
   declarations: [
     AppComponent
